2cf02cfe81aafd90b2388c1527aa01b3b1c484fa
amandakremser <amandakresmser@gmail.com>
2015-05-26 21:23:21 -0700

    set up plugin info

--- bizzy-form-plugin.php
+++ bizzy-form-plugin.php
@@ -1,8 +1,15 @@
 <?php
 
 /**
-*Plugin Name: Bizzy Form Plugin
-*Plugin URI: http://amandkremser.com
-*Author: Amanda Kremser
+*Plugin Name: Bizzy Email Sign-up Form Creator
+*Plugin URI: http://www.bizzy.io
+*Description: A plugin to genrate sign up forms for Bizzy email lists.
+*Version:     0.1
+*Author:      Amanda Kremser
+*Author URI:  http://www.amandakremser.com
+*License:     GPL2
+*License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*Domain Path: /languages
+*Text Domain: my-toolset
 */
 
